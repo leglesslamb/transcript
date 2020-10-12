e510df789bd15d5f5d840e3991efc162b6490b25
leglesslamb <wu.rupert@outlook.com>
2020-10-11 22:08:50 -0400

    Added flag/argument matching for binary.

--- src/bin.rs
+++ src/bin.rs
@@ -1,1 +1,15 @@
-pub fn main() {}
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let result = match (args[1].as_str(), args.len()) {
+        ("-h", 2) => "Help; not implemented!",
+        ("-lb", 3) => "Latin => Braille; not implemented!",
+        ("-lc", 3) => "Latin => Cyrillic; not implemented!",
+        ("-lg", 3) => "Latin => Greek; not implemented!",
+        ("-lr", 3) => "Latin => Runic; not implemented!",
+        _ => "Invalid transcription rule. Use -h for help",
+    };
+
+    println!("{}", result);
+}
