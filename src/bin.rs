9fd25155cf14f7fdb7dc293d024a438a48c0f84a
leglesslamb <wu.rupert@outlook.com>
2020-10-11 22:27:23 -0400

    Added input validation and using String instead of &str.

--- src/bin.rs
+++ src/bin.rs
@@ -1,14 +1,21 @@
 use std::env;
 
+const INVALID: &str = "Invalid transcription rule. Use -h for help";
+const NO_ARGS: &str = "No arguments given. Use -h for help";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("{}", NO_ARGS);
+        return;
+    }
     let result = match (args[1].as_str(), args.len()) {
-        ("-h", 2) => "Help; not implemented!",
-        ("-lb", 3) => "Latin => Braille; not implemented!",
-        ("-lc", 3) => "Latin => Cyrillic; not implemented!",
-        ("-lg", 3) => "Latin => Greek; not implemented!",
-        ("-lr", 3) => "Latin => Runic; not implemented!",
-        _ => "Invalid transcription rule. Use -h for help",
+        ("-h", 2) => "Help; not implemented!".to_string(),
+        ("-lb", 3) => "Latin => Braille; not implemented!".to_string(),
+        ("-lc", 3) => "Latin => Cyrillic; not implemented!".to_string(),
+        ("-lg", 3) => "Latin => Greek; not implemented!".to_string(),
+        ("-lr", 3) => "Latin => Runic; not implemented!".to_string(),
+        _ => INVALID.to_string(),
     };
 
     println!("{}", result);
